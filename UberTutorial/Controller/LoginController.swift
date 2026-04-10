c09423a382ad396387a75671798769eb2b48d484
Yonghyeon Kim <yonghyeon.dev@gmail.com>
2026-04-10 20:09:15 +0900

    Initial commit

---
+++ UberTutorial/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  UberTutorial
+//
+//  Created by Rondo on 4/10/26.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
