aa42803d23449d3152e58241eb36c4313d1dc381
Yonghyeon Kim <yonghyeon.dev@gmail.com>
2026-04-10 20:35:18 +0900

    Programmatic Intro To UI

--- UberTutorial/Controller/LoginController.swift
+++ UberTutorial/Controller/LoginController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    // MARK: - Properties
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+
+        view.addSubview(titleLabel)
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
     }
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 }
